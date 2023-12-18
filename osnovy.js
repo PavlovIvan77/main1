a5cf25fb8489d37ab99c253adf9c07f03c16e167
PavlovIvan77 <130969623+PavlovIvan77@users.noreply.github.com>
2023-12-18 11:43:03 +0300

    Add files via upload

---
+++ osnovy.js
@@ -0,0 +1,20 @@
+// let a = "Hello, World!"
+// let user = {
+//     name: "Дима",
+//     age: 17
+// }
+// let b = "abc"
+// a = 10
+// const A = (text) => {
+//      alert(text)
+// }
+// A("hello")
+let s = 1
+for (let i = 2; i <= 5; i++){
+    s = s * i
+}
+// let name = prompt('Кто ты?')
+alert(s)
+// let age = prompt('Насколько ты стар?')
+// alert(`Вау, тебе уже ${age}`)
+// alert(a)
